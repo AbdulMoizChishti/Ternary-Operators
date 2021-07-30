34ed56f594f24e0a2822fba92a0b26c6fc9aeeb0
AbdulMoizChishti <moaiz599@gmail.com>
2021-07-30 13:32:33 +0500

    Ternary Operators

---
+++ src/ternaryoperators/TernaryOperators.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ternaryoperators;
+
+/**
+ *
+ * @author Abdul Moiz Chishti
+ */
+public class TernaryOperators {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int a=20;
+        boolean num = (a==20) ? true:false ;
+        System.out.println(num);
+        
+        
+        
+        
+    }
+    
+}
